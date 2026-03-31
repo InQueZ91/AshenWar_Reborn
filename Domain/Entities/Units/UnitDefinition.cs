cca6d5095b497313828309063f933d1d0aacd662
QueZ <inquez91@gmail.com>
2026-03-31 18:47:42 +0700

    feat(domain): add unit and ability definition models

--- Domain/Entities/Units/UnitDefinition.cs
+++ Domain/Entities/Units/UnitDefinition.cs
@@ -1,14 +1,23 @@
-﻿using Domain.ValueObjects;
+﻿using System;
+using System.Collections.Generic;
+using Domain.Entities.Abilities;
+using Domain.ValueObjects;
 using Domain.ValueObjects.Identifiers;
 
-namespace Domain.Entities.Unit;
+namespace Domain.Entities.Units;
 
 public sealed class UnitDefinition : Entity
 {
-    public UnitDefinitionId Id { get; private set; }
-    public VisualId VisualId { get; private set; }
+    // Identifiers
+    public UnitDefinitionId Id { get; init; }
     public string Name { get; private set; }
+    
+    // References
+    public VisualId VisualId { get; private set; }
+    
+    // Properties
     public UnitStats BaseStats { get; private set; }
+    public List<AbilityDefinition> Abilities { get; private set; } = new List<AbilityDefinition>();
 
     // Constructor
     private UnitDefinition(VisualId visualId, string name, UnitStats baseStats)
@@ -22,4 +31,12 @@ public sealed class UnitDefinition : Entity
     {
         return new UnitDefinition(visualId, name, baseStats);
     }
+    
+    // Methods
+    public void ChangeName(string newName) => Name = newName;
+    public void ChangeVisualId(VisualId visualId) => VisualId = visualId;
+    public void SetBaseStats(UnitStats newBaseStats) => BaseStats = newBaseStats;
+
+    public void AddAbility(AbilityDefinition abilityDefinition) => Abilities.Add(abilityDefinition);
+    public void RemoveAbility(AbilityDefinitionId abilityDefinitionId) => Abilities.RemoveAll(a => a.Id == abilityDefinitionId);
 }
